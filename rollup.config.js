f4f01ab2264ca548c164bad980a69f7bd0c00d81
Daniel Bultas <danielbultas@gmail.com>
2022-04-27 14:46:45 +0200

    use default chunks

--- rollup.config.js
+++ rollup.config.js
@@ -3,17 +3,10 @@ import resolve from "@rollup/plugin-node-resolve";
 import replace from "@rollup/plugin-replace";
 import serve from "rollup-plugin-serve";
 
-const getScripts = dir =>
-  fs.readdirSync(dir).map(file => {
+const getScripts = (dir) =>
+  fs.readdirSync(dir).map((file) => {
     return `${dir}${file}`;
   });
-
-const getModuleName = directory => path => {
-  const parts = path.split("/");
-  const index = parts.findIndex(e => e === directory) + 1;
-  const name = parts[index];
-  return name;
-};
 
 export default {
   input: getScripts("./scripts/"),
@@ -21,28 +14,20 @@ export default {
     dir: "./public/scripts/",
     format: "es",
     chunkFileNames: "[name].js", // "[name]-[hash].js"
-    sourcemap: true
-  },
-  manualChunks(id) {
-    if (id.includes("node_modules")) {
-      return getModuleName("node_modules")(id);
-    }
-
-    if (id.includes("custom_modules")) {
-      return getModuleName("custom_modules")(id);
-    }
+    sourcemap: true,
   },
   plugins: [
     resolve(),
     replace({
-      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)
+      preventAssignment: true,
+      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
     }),
     serve({
       open: true,
       contentBase: ["public"],
       headers: {
         // "Cache-Control": "max-age=3600"
-      }
-    })
-  ]
+      },
+    }),
+  ],
 };
